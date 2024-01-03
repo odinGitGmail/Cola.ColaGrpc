7c0524e3b0c6bb50ca58c5e837b0efd8707a7981
Din Ya <>
2024-01-03 17:04:41 +0800

    init cola.grpc

---
+++ IPC/NamedPipesConnectionFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Pipes;
+using System.Security.Principal;
+
+namespace Cola.ColaGrpc.IPC;
+
+public class NamedPipesConnectionFactory
+{
+    private readonly string _pipeName;
+
+    public NamedPipesConnectionFactory(string pipeName)
+    {
+        _pipeName = pipeName;
+    }
+
+    public async ValueTask<Stream> ConnectAsync(SocketsHttpConnectionContext _,
+        CancellationToken cancellationToken = default)
+    {
+        var clientStream = new NamedPipeClientStream(
+            serverName: ".",
+            pipeName: _pipeName,
+            direction: PipeDirection.InOut,
+            options: PipeOptions.WriteThrough | PipeOptions.Asynchronous,
+            impersonationLevel: TokenImpersonationLevel.Anonymous);
+        try
+        {
+            await clientStream.ConnectAsync(cancellationToken).ConfigureAwait(false);
+            return clientStream;
+        }
+        catch
+        {
+            clientStream.Dispose();
+            throw;
+        }
+    }
+}
